05b116a5682b2c0a9897252c8555120590770c4d
mohdsaifansari191-art <mohdsaifansari191@gmail.com>
2026-08-13 21:08:02 +0530

    style adding on components

--- react-components/src/App.jsx
+++ react-components/src/App.jsx
@@ -1,10 +1,12 @@
 import Hello from "./hello";
 import MyButton from "./MyButton";
+import Random from "./Random";
 function App() {
   return <div>
     <h1>hello world</h1>
     <MyButton></MyButton>
     <Hello></Hello>
+    <Random></Random>
   </div>
 
 }
--- react-components/src/random.jsx
+++ react-components/src/random.jsx
@@ -1,3 +1,6 @@
-function random(){
-    
+function Random(){
+    let number = Math.random()*100
+    return<h1 style ={{"background-color":"#235612"}}>This is rendom number :{Math.round(number)}</h1>
 }
+
+export default Random;
