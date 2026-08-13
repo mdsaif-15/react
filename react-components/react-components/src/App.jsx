40a72cfd462ff2642a5a0709134451918741204c
mohdsaifansari191-art <mohdsaifansari191@gmail.com>
2026-08-13 11:16:13 +0530

    Dynamic Components

--- react-components/react-components/src/App.jsx
+++ react-components/react-components/src/App.jsx
@@ -1,8 +1,10 @@
+import Hello from "./hello";
 import MyButton from "./MyButton";
 function App() {
   return <div>
     <h1>hello world</h1>
     <MyButton></MyButton>
+    <Hello></Hello>
   </div>
 
 }
---
+++ react-components/react-components/src/Hello.jsx
@@ -0,0 +1,10 @@
+function Hello() {
+    let name = "saif";
+
+    let fullName = () => {
+        return "MD Saif"
+    }
+    return <p> Hello this is {name} </p>;
+}
+
+export default Hello;
