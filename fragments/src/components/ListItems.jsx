64b3de4f31a44e01b662725e8c4aab3f58db29af
mohdsaifansari191-art <mohdsaifansari191@gmail.com>
2026-08-14 20:21:28 +0530

    one element added in array

--- fragments/src/components/ListItems.jsx
+++ fragments/src/components/ListItems.jsx
@@ -1,5 +1,5 @@
 function ListItems() {
-    let foodItems = ["Dal", "Green Vegetable", "Roti", "Salad", "Milk"];
+    let foodItems = ["Dal", "Green Vegetable", "Roti", "Salad", "Milk", "ghee"];
     return (
         <ul class="list-group">
             {foodItems.map(item => <li class="list-group-item active">{item}</li>)}
