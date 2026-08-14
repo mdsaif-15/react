65b8750f39b3cd96bf7c6d723786e3069a5dd251
mohdsaifansari191-art <mohdsaifansari191@gmail.com>
2026-08-14 20:20:44 +0530

    array of food Items render by map

--- fragments/src/components/ListItems.jsx
+++ fragments/src/components/ListItems.jsx
@@ -1,11 +1,9 @@
 function ListItems() {
+    let foodItems = ["Dal", "Green Vegetable", "Roti", "Salad", "Milk"];
     return (
         <ul class="list-group">
-            <li class="list-group-item active" aria-current="true">An active item</li>
-            <li class="list-group-item">A second item</li>
-            <li class="list-group-item">A third item</li>
-            <li class="list-group-item">A fourth item</li>
-            <li class="list-group-item">And a fifth one</li>
+            {foodItems.map(item => <li class="list-group-item active">{item}</li>)}
+
         </ul>);
 }
 
