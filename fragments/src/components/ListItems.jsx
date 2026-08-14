f2f759a8669963ff1fefd72f3d22d61d2c590234
mohdsaifansari191-art <mohdsaifansari191@gmail.com>
2026-08-14 20:47:15 +0530

    conditional rendering

--- fragments/src/components/ListItems.jsx
+++ fragments/src/components/ListItems.jsx
@@ -1,8 +1,18 @@
 function ListItems() {
+    // "Dal", "Green Vegetable", "Roti", "Salad", "Milk", "ghee"
+    //let foodItems = [];
     let foodItems = ["Dal", "Green Vegetable", "Roti", "Salad", "Milk", "ghee"];
+
+    if (foodItems.length === 0) {
+        return (<p>
+            I am still hungry
+
+        </p>
+        );
+    }
     return (
         <ul class="list-group">
-            {foodItems.map(item => <li class="list-group-item active">{item}</li>)}
+            {foodItems.map(item => <li key={item} class="list-group-item active">{item}</li>)}
 
         </ul>);
 }
