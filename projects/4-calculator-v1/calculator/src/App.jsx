f78fe26cd2d188c03ae91440e960b1125a340faa
mohdsaifansari191-art <mohdsaifansari191@gmail.com>
2026-09-09 21:21:18 +0530

    fragement

---
+++ projects/4-calculator-v1/calculator/src/App.jsx
@@ -0,0 +1,10 @@
+// import { Button } from "bootstrap"
+
+const App = () => {
+    return(
+    <div className="calculator">
+        <input className="Display">Display</input>
+        <Button className="Button">press</Button>
+    </div>);
+}
+export default App;
